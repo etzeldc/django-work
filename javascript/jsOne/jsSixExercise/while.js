3dcd040ca3de5f11b13eadb9d15f1144f933efec
David Etzel <etzeldc@gmail.com>
2017-08-19 11:24:25 -0600

    Second JS Commit
    JS Level One Exercise: While loop.
    Retroactive file organization.

---
+++ javascript/jsOne/jsSixExercise/while.js
@@ -0,0 +1,12 @@
+// QUICK EXERCISE
+
+// Write a while loop that prints out only the even numbers from 1 to 10.
+
+var x = 0;
+
+while (x < 11) {
+    x += 1;
+    if (x % 2 === 0) {
+        console.log(x);
+    }
+}
